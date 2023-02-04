4c054d0d75542abd0c66fe08de95098c852b9ac6
Guilherme Roque <halversonhitori@gmail.com>
2023-02-04 12:04:31 -0400

    api retrofit adicionado bem basico ao botao buscar

--- app/src/main/java/com/example/hereis/MainActivity.java
+++ app/src/main/java/com/example/hereis/MainActivity.java
@@ -9,11 +9,13 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 Button btnproximo;
+Button btnbuscar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnproximo = findViewById(R.id.btnproximo);
+        btnbuscar = findViewById(R.id.btnbuscar);
         btnproximo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -21,5 +23,15 @@ Button btnproximo;
           startActivity(atimap);
             }
         });
-    }
-}
+
+     btnbuscar.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent retro =new Intent(getApplicationContext(),MapsActivityretro.class);
+            startActivity(retro);
+                                       }
+                                                              }  );
+
+                                                        }
+                                                    }
+
--- app/src/main/java/com/example/hereis/MapsActivity.java
+++ app/src/main/java/com/example/hereis/MapsActivity.java
@@ -1,8 +1,21 @@
 package com.example.hereis;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,17 +25,24 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.hereis.databinding.ActivityMapsBinding;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
-
+    private String [] permissoes = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+    private LocationManager locationManager;
+    private LocationListener locationListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        Permissoes.validarPermissoes(permissoes, this, 1);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -43,9 +63,65 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Log.d("Localização", "onLocationChanged" + location.toString());
+                Double latitude = location.getLatitude();
+                Double longitude = location.getLongitude();
+                mMap.clear();
+                LatLng localUsuario = new LatLng(latitude, longitude);
+                mMap.addMarker(new MarkerOptions().position(localUsuario).title("Estou aqui!"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(localUsuario, 15));
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try{
+                    List<Address> listaEndereco = geocoder.getFromLocation(latitude, longitude, 1);
+                    if (listaEndereco != null && listaEndereco.size() > 0){
+                        Address endereco = listaEndereco.get(0);
+                        Log.d("Endereço", "onLocationChanged" + endereco.toString());
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        pedirPermissao();}
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for (int permissaoResultado : grantResults){
+            if(permissaoResultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacao();
+            }else if (permissaoResultado == PackageManager.PERMISSION_GRANTED){
+                pedirPermissao();
+            }
+        }
+    } private void pedirPermissao(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    0,
+                    0,
+                    locationListener);
+        }
     }
+
+    private void alertaValidacao(){
+        AlertDialog.Builder alerta = new AlertDialog.Builder(getApplicationContext());
+        alerta.setTitle("Permissões negadas!");
+        alerta.setMessage("Para utilizar o app é necessário aceitar as permissões!");
+        alerta.setCancelable(false);
+        alerta.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        alerta.create().show();
+    }
+
 }
